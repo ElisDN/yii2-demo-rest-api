2b42f514c2f22816d4a7bd989c2c34daf73b66f4
ElisDN <mail@elisdn.ru>
2017-02-15 15:10:05 +0300

    Changed URL rules

--- api/config/main.php
+++ api/config/main.php
@@ -49,9 +49,7 @@ return [
             'rules' => [
                 '' => 'site/index',
                 'auth' => 'site/login',
-                '<_c:[\w-]+>' => '<_c>/index',
-                '<_c:[\w-]+>/<id:\d+>' => '<_c>/view',
-                '<_c:[\w-]+>/<id:\d+>/<_a:[\w-]+>' => '<_c>/<_a>',
+                'GET profile' => 'profile/index',
             ],
         ],
     ],
